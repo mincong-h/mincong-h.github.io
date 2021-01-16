4f59dc94ceefe1c7e4239d04402389aa76a63653
Mincong HUANG <mincong.h@gmail.com>
2021-01-16 09:50:14 +0100

    Revert undesired changes

--- docker-serve.sh
+++ docker-serve.sh
@@ -4,10 +4,9 @@
 # https://github.com/envygeeks/jekyll-docker
 #
 export JEKYLL_VERSION=3.8
-docker run \
-  --rm \
+docker run --rm \
   -p 4000:4000 \
-  --volume "${PWD}:/srv/jekyll" \
-  --volume "${PWD}/vendor/bundle:/usr/local/bundle" \
-  jekyll/jekyll:$JEKYLL_VERSION \
+  --volume="${PWD}:/srv/jekyll" \
+  --volume="${PWD}/vendor/bundle:/usr/local/bundle" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve $@
