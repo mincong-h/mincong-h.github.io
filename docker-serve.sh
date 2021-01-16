5fafa3c71742f525811698c0fa9708a5c40a44a8
Mincong Huang <mincong.h@gmail.com>
2021-01-16 09:52:36 +0100

    Make jekyll-serve faster (#13)
    * Make the build faster
    
    * Update README
    
    * Revert undesired changes

--- docker-serve.sh
+++ docker-serve.sh
@@ -7,5 +7,6 @@ export JEKYLL_VERSION=3.8
 docker run --rm \
   -p 4000:4000 \
   --volume="${PWD}:/srv/jekyll" \
+  --volume="${PWD}/vendor/bundle:/usr/local/bundle" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve
+  jekyll serve $@
