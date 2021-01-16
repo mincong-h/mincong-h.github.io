18e01bbc9eabb87b595bbe1fe50ba9684ce5da8e
Mincong HUANG <mincong.h@gmail.com>
2021-01-16 09:14:36 +0100

    Make the build faster

--- docker-serve.sh
+++ docker-serve.sh
@@ -4,8 +4,10 @@
 # https://github.com/envygeeks/jekyll-docker
 #
 export JEKYLL_VERSION=3.8
-docker run --rm \
+docker run \
+  --rm \
   -p 4000:4000 \
-  --volume="${PWD}:/srv/jekyll" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve
+  --volume "${PWD}:/srv/jekyll" \
+  --volume "${PWD}/vendor/bundle:/usr/local/bundle" \
+  jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll serve $@
