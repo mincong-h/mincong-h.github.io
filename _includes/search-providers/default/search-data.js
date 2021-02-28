170529e6f5a69c0d45b8f88d29c98dcf09f1725b
Mincong HUANG <mincong.h@gmail.com>
2021-02-28 21:59:58 +0100

    Display label for displayed_categories

--- _includes/search-providers/default/search-data.js
+++ _includes/search-providers/default/search-data.js
@@ -1,7 +1,7 @@
 window.TEXT_SEARCH_DATA={
   {%- for _collection in site.collections -%}
     {%- unless forloop.first -%},{%- endunless -%}
-    '{{ _collection.label }}':[
+    '{{ _collection.my_label | default: _collection.label }}':[
       {%- for _article in _collection.docs -%}
       {%- unless forloop.first -%},{%- endunless -%}
       {'title':{{ _article.title | jsonify }},
