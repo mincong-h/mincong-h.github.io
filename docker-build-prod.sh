f6153513f787e4a3f6b98ce079d85b798ce838fa
Mincong HUANG <mincong.h@gmail.com>
2021-07-14 14:10:25 +0200

    Remove port

--- docker-build-prod.sh
+++ docker-build-prod.sh
@@ -5,7 +5,6 @@
 #
 export JEKYLL_VERSION=3.8
 docker run --rm \
-  -p 4000:4000 \
   --env JEKYLL_ENV=production \
   --volume="${PWD}:/srv/jekyll" \
   --volume="${PWD}/vendor/bundle:/usr/local/bundle" \
