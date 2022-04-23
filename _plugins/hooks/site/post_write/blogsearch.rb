a92a6711e92a886da068a0d22db714043aae11e6
Mincong HUANG <mincong.h@gmail.com>
2022-04-23 08:09:55 +0200

    print post

--- _plugins/hooks/site/post_write/blogsearch.rb
+++ _plugins/hooks/site/post_write/blogsearch.rb
@@ -1,3 +1,6 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-  Jekyll.logger.info "Updating blog posts to BlogSearch..."
+    Jekyll.logger.info "Updating blog posts to BlogSearch..."
+    site.posts.docs.each { |post|
+        Jekyll.logger.info post.data['title']
+    }
 end
