0d0202b6bb97b071f20ef78059854d204498fd9b
Mincong HUANG <mincong.h@gmail.com>
2022-04-23 09:25:25 +0200

    Add date

--- _plugins/hooks/site/post_write/blogsearch.rb
+++ _plugins/hooks/site/post_write/blogsearch.rb
@@ -1,6 +1,10 @@
 Jekyll::Hooks.register :site, :post_write do |site|
     Jekyll.logger.info "Updating blog posts to BlogSearch..."
+
+    # See more variables in https://jekyllrb.com/docs/variables/
     site.posts.docs.each { |post|
-        Jekyll.logger.info post.data['title']
+        # Jekyll.logger.info post.data["date"].class
+        Jekyll.logger.info post.data["date"].strftime("%FT%T%:z")
+        Jekyll.logger.info post.data["title"].strip
     }
 end
