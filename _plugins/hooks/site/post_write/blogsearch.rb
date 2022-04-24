c21c07025ed790486d2950625a03959ac6c7cb2a
Mincong HUANG <mincong.h@gmail.com>
2022-04-24 10:07:59 +0200

    Send HTTP request

--- _plugins/hooks/site/post_write/blogsearch.rb
+++ _plugins/hooks/site/post_write/blogsearch.rb
@@ -1,5 +1,16 @@
+require 'net/http'
+
 Jekyll::Hooks.register :site, :post_write do |site|
     Jekyll.logger.info "Updating blog posts to BlogSearch..."
+    Jekyll.logger.info ENV["JIMI_USERNAME"]
+    Jekyll.logger.info ENV["JIMI_PASSWORD"]
+
+    Jekyll.logger.info "Getting information from Jimi Data"
+    uri = URI('https://search.jimidata.info')
+    response = Net::HTTP.get_response(uri)
+    Jekyll.logger.info response.code
+    Jekyll.logger.info response.body
+    Jekyll.logger.info "==="
 
     # See more variables in https://jekyllrb.com/docs/variables/
     site.posts.docs.each { |post|
