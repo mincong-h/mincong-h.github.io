79e8cc92c597655b68dec3f5c0de8a9a245d903b
mincong-h <mincong.h@gmail.com>
2022-04-25 19:58:02 +0000

    Deploying to gh-pages from @ mincong-h/mincong-h.github.io@e425f27c2cb3452f5e122a16b57757a285bc1bfa 🚀

--- docker-serve.sh
+++ docker-serve.sh
@@ -10,7 +10,10 @@
 export JEKYLL_VERSION=3.8
 docker run --rm \
   -p 4000:4000 \
-  --volume="${PWD}:/srv/jekyll" \
-  --volume="${PWD}/vendor/bundle:/usr/local/bundle" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
+  --env "JIMI_USERNAME=${JIMI_USERNAME}" \
+  --env "JIMI_PASSWORD=${JIMI_PASSWORD}" \
+  --name jekyll \
+  --volume "${PWD}:/srv/jekyll" \
+  --volume "${PWD}/vendor/bundle:/usr/local/bundle" \
+  -it "jekyll/jekyll:${JEKYLL_VERSION}" \
   jekyll serve $@
