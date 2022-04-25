e425f27c2cb3452f5e122a16b57757a285bc1bfa
Mincong Huang <mincong.h@gmail.com>
2022-04-25 21:54:52 +0200

    Add Jekyll plugin for BlogSearch (#56)
    * Add plugin structure
    
    * print post
    
    * Add date
    
    * Add url
    
    * Fix url
    
    * Send HTTP request
    
    * Write to Jimi search
    
    * Fix body
    
    * Add vars in CI

---
+++ _plugins/hooks/site/post_write/blogsearch.rb
@@ -0,0 +1,42 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+Jekyll::Hooks.register :site, :post_write do |site|
+    Jekyll.logger.info "Updating blog posts to BlogSearch..."
+    username = ENV["JIMI_USERNAME"]
+    password = ENV["JIMI_PASSWORD"]
+
+    site_info = Net::HTTP.get URI('https://search.jimidata.info')
+    Jekyll.logger.info site_info
+
+    # See more variables in https://jekyllrb.com/docs/variables/
+    site.posts.docs.each { |post|
+        url = post.url
+        title = post.data["title"]
+        content = post.content
+        Jekyll.logger.info "Indexing post: " + title + " (" + post.id + ")"
+
+        pos = post.id.rindex('/') + 1
+        postId = post.id[pos..-1]  # hack: remove prefix
+        uri = URI.parse('https://search.jimidata.info/sites/mincong.io/posts/' + postId)
+        Jekyll.logger.info uri
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        headers = {"Content-Type": "application/json"}
+        body = {"title" => title, "url" => url, "content" => content}.to_json
+        Jekyll.logger.info body
+
+        request = Net::HTTP::Put.new(uri.request_uri, headers)
+        request.basic_auth username, password
+        request.body = body
+
+        response = http.request(request)
+
+        Jekyll.logger.info response.code
+        Jekyll.logger.info response.body
+        Jekyll.logger.info "---"
+    }
+end
