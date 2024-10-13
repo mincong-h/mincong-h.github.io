ae42d8880e8ed9a3f96890eb9e324c5c6ea10f72
Mincong Huang <mincong.h@gmail.com>
2024-10-13 21:30:24 +0200

    Add slides of the k8s course - chapter 1 (#114)

---
+++ esigelec/1/assets/slidev/default-DNcY6MkN.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-D1L0g39B.js";import{_ as r}from"../index-BzXCSCr6.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
