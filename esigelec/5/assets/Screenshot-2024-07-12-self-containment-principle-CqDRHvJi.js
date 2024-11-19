aabfc8dd53f49d094bd3870627e3ea6f241deb74
Mincong Huang <mincong.h@gmail.com>
2024-11-19 22:37:57 +0100

    Add slides for chapter 5

---
+++ esigelec/5/assets/Screenshot-2024-07-12-self-containment-principle-CqDRHvJi.js
@@ -0,0 +1,1 @@
+const e="/esigelec/5/assets/Screenshot-2024-07-12-self-containment-principle-cvPel9aI.png";export{e as _};
---
+++ esigelec/5/assets/slidev/center-D-h81nxk.js
@@ -0,0 +1,1 @@
+import{o as t,b as o,e as s,f as c}from"../modules/vue-DXUHFyMa.js";import{_ as r}from"../index-BVMKZixe.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return t(),o("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
